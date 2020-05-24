ca65718b491d820d2e86fb5dc78cf477fb7d0b0d
rawanfatima <rawanfatima@outlook.com>
2020-05-24 14:08:02 -0700

    completed server.js file

--- server.js
+++ server.js
@@ -12,3 +12,90 @@ const app = express();
 
 //parse incoming strng or array data
 
+app.use(express.urlencoded ( { extended: true}));
+
+// parse incoming JSON data
+
+app.use(express.json());
+
+//public files
+
+app.use (express.static('public'));
+
+//request data
+
+const { notes } = require('./data/db.json');
+
+//handling data from req.body
+
+function createNewNote (body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+
+    //path to write files
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/db.json'),
+        JSON.stringify({ notes : notesArray }, null, 2)
+    );
+
+    return note;
+};
+
+//validating data
+
+function validataNote (note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== "string") {
+        return false;
+    }
+    return true;
+};
+
+//route GET
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+// routing to server to accept data to be used/stores serve-side
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    //validating data
+
+    if(!validateNote(req.body)) {
+        res.status(400).send('The note is not formatted correctly');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+//deleting notes
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    let note;
+
+    notes.map((element, index) => {
+        if(element.id == id) {
+            note = element
+            notes.splice(index, 1)
+            return res.json(note);
+        }
+    })
+});
+
+//routing to index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//chain liste() method to our servers
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
